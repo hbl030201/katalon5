fa647aee7e1d7f5013370d409d9d6435249f1385
mars.wu <hbl030201@gmail.com>
2023-12-23 22:13:22 +0800

    update python path1

--- CB_web_Sanity/Flowting_board/Sticker/c187581.bat
+++ CB_web_Sanity/Flowting_board/Sticker/c187581.bat
@@ -1,1 +1,1 @@
-C:\Python37\python.exe "C:\Users\mars.wu\Katalon Studio\CBWeb_1_0_jenkins\CB_web_Sanity\Flowting_board\Sticker\c187581.py"
+C:\Python37\python.exe "CBWeb_1_0_jenkins\CB_web_Sanity\Flowting_board\Sticker\c187581.py"
